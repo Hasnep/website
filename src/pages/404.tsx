e243dad0244d86b782f2a5a9224a74890810b015
Hannes <hasnep@gmail.com>
2020-05-25 13:34:24 +0100

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import React, { Fragment, ReactElement } from "react";
+
+const FourOhFourPage = (): ReactElement => (
+  <Fragment>
+    <h1>Error 404</h1>
+    <h2>:(</h2>
+  </Fragment>
+);
+
+export default FourOhFourPage;
