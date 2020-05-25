49c9d925380302ddd95afcfd14ac3c80aa472bec
Hannes <hasnep@gmail.com>
2020-05-25 13:33:34 +0100

    Add gatsby config files

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: "Hannes' Website",
+    description: "My website.",
+    author: "Hannes",
+  },
+  plugins: [
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-sass",
+    // "gatsby-plugin-graphql-codegen",
+  ],
+};
