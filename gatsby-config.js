4526d823252ee054a00bf52f46fe2825fb106335
Hannes <hasnep@gmail.com>
2020-06-02 18:30:22 +0100

    Move gatsby config to tsx files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,5 @@
-module.exports = {
-  siteMetadata: {
-    title: "Hannes' Website",
-    description: "My website.",
-    author: "Hannes",
-  },
-  plugins: [
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-sass",
-    // "gatsby-plugin-graphql-codegen",
-  ],
-};
+// We register the TypeScript evaluator in gatsby-config so we don't need to do it in any other .js file. It automatically reads TypeScript config from tsconfig.json.
+require("ts-node").register();
+
+// Use a TypeScript version of gatsby-config.js.
+module.exports = require("./gatsby-config.ts");
---
+++ gatsby-config.ts
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import fs from "fs";
+
+dotenv.config({ path: ".env" });
+
+export const siteMetadata = {
+  title: "Ha.nnesSm.it",
+  description: "My website to show off some of my projects.",
+  author: "Hannes",
+};
+
+export const plugins = [
+  "gatsby-plugin-typescript",
+  "gatsby-plugin-react-helmet",
+  {
+    resolve: "gatsby-source-github-api",
+    options: {
+      token: `${process.env.GITHUB_API_TOKEN}`,
+      variables: { username: "Hasnep" },
+      graphQLQuery: fs.readFileSync("./src/github.graphql").toString(),
+    },
+  },
+];
