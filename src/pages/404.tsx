fb60f7e4bbf90e9dd07faf2b3537e6d13d3f034a
Hannes <hasnep@gmail.com>
2020-06-02 18:35:11 +0100

    Make 404 page use page component

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,11 @@
-import React, { Fragment, ReactElement } from "react";
+import React from "react";
+import Page from "../components/page";
 
-const FourOhFourPage = (): ReactElement => (
-  <Fragment>
+const FourOhFourPage = (): JSX.Element => (
+  <Page>
     <h1>Error 404</h1>
     <h2>:(</h2>
-  </Fragment>
+  </Page>
 );
 
 export default FourOhFourPage;
