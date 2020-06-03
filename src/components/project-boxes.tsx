4a00092226aac6a267d0d9cd9f48fa42c404b004
Hannes <hasnep@gmail.com>
2020-06-03 22:27:25 +0100

    Move projects to a page

--- src/components/project-boxes.tsx
+++ src/components/project-boxes.tsx
@@ -16,7 +16,7 @@ const ProjectBox = (props: {
 }): JSX.Element => {
   return (
     <div className={"box box-hover"}>
-        <Link to={"/projects/" + slugify(props.name)}>
+      <Link to={"/projects/" + slugify(props.name)}>
         <h2>{props.name}</h2>
         <p>
           <Dot colour={props.language.colour} /> {props.language.name}
@@ -27,62 +27,4 @@ const ProjectBox = (props: {
   );
 };
 
-const ProjectBoxes = (): JSX.Element => (
-  <StaticQuery
-    query={graphql`
-      query GetGithubProjectSummaries {
-        githubData {
-          data {
-            viewer {
-              repositories {
-                nodes {
-                  name
-                  languages {
-                    edges {
-                      node {
-                        name
-                        color
-                      }
-                    }
-                  }
-                  description
-                  createdAt
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data: {
-      githubData: {
-        data: { viewer: { repositories: { nodes: IProjectInfo[] } } };
-      };
-    }): JSX.Element => (
-      <Fragment>
-        {data.githubData.data.viewer.repositories.nodes
-          .filter((repo_info: IProjectInfo) => !repo_info.isArchived)
-          .filter(
-            (repo_info: IProjectInfo) => repo_info.languages.edges.length > 0
-          )
-          .map((repo_info: IProjectInfo) => {
-            const language_info = repo_info.languages.edges[0].node;
-            return (
-              <Fragment key={slugify(repo_info.name)}>
-                <ProjectBox
-                  name={repo_info.name}
-                  language={{
-                    name: language_info.name,
-                    colour: language_info.color,
-                  }}
-                  description={repo_info.description}
-                />
-              </Fragment>
-            );
-          })}
-      </Fragment>
-    )}
-  />
-);
-
-export default ProjectBoxes;
+export default ProjectBox;
---
+++ src/pages/projects.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Page from "../components/page";
+import ProjectBox from "../components/project-boxes";
+import { graphql, StaticQuery } from "gatsby";
+
+const ProjectsPage = (): JSX.Element => (
+  <Page>
+    <StaticQuery
+      query={graphql`
+        query GetGithubProjectSummaries {
+          githubData {
+            data {
+              viewer {
+                repositories {
+                  nodes {
+                    name
+                    languages {
+                      edges {
+                        node {
+                          name
+                          color
+                        }
+                      }
+                    }
+                    description
+                    createdAt
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={(data: {
+        githubData: {
+          data: { viewer: { repositories: { nodes: IProjectInfo[] } } };
+        };
+      }): JSX.Element => (
+        <Fragment>
+          {data.githubData.data.viewer.repositories.nodes
+            .filter((repo_info: IProjectInfo) => !repo_info.isArchived)
+            .filter(
+              (repo_info: IProjectInfo) => repo_info.languages.edges.length > 0
+            )
+            .map((repo_info: IProjectInfo) => {
+              const language_info = repo_info.languages.edges[0].node;
+              return (
+                <Fragment key={slugify(repo_info.name)}>
+                  <ProjectBox
+                    name={repo_info.name}
+                    language={{
+                      name: language_info.name,
+                      colour: language_info.color,
+                    }}
+                    description={repo_info.description}
+                  />
+                </Fragment>
+              );
+            })}
+        </Fragment>
+      )}
+    />
+  </Page>
+);
+
+export default ProjectsPage;
