7d039492fa59647b185786e8161271867615a570
Hannes <hasnep@gmail.com>
2020-07-22 22:38:24 +0100

    Change projects to pagequery from staticquery

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,65 +1,69 @@
-import React from "react";
+import { graphql } from "gatsby";
 import Page from "../components/page";
+import PageTitle from "../components/page-title";
 import ProjectBox from "../components/project-box";
-import { graphql, StaticQuery } from "gatsby";
+import React, { Fragment } from "react";
+import slugify from "slugify";
 
-const ProjectsPage = (): JSX.Element => (
-  <Page>
-    <StaticQuery
-      query={graphql`
-        query GetGithubProjectSummaries {
-          githubData {
-            data {
-              viewer {
-                repositories {
-                  nodes {
+export const query = graphql`
+  query GetGithubProjectSummaries {
+    githubData {
+      data {
+        viewer {
+          repositories {
+            nodes {
+              name
+              languages {
+                edges {
+                  node {
                     name
-                    languages {
-                      edges {
-                        node {
-                          name
-                          color
-                        }
-                      }
-                    }
-                    description
-                    createdAt
+                    color
                   }
                 }
               }
+              description
+              createdAt
             }
           }
         }
-      `}
-      render={(data: {
-        githubData: {
-          data: { viewer: { repositories: { nodes: IProjectInfo[] } } };
-        };
-      }): JSX.Element => (
-        <Fragment>
-          {data.githubData.data.viewer.repositories.nodes
-            .filter((repo_info: IProjectInfo) => !repo_info.isArchived)
-            .filter(
-              (repo_info: IProjectInfo) => repo_info.languages.edges.length > 0
-            )
-            .map((repo_info: IProjectInfo) => {
-              const language_info = repo_info.languages.edges[0].node;
-              return (
-                <Fragment key={slugify(repo_info.name)}>
-                  <ProjectBox
-                    name={repo_info.name}
-                    language={{
-                      name: language_info.name,
-                      colour: language_info.color,
-                    }}
-                    description={repo_info.description}
-                  />
-                </Fragment>
-              );
-            })}
-        </Fragment>
-      )}
-    />
+      }
+    }
+  }
+`;
+
+const repo_info_to_page = (repo_info: IProjectInfo): JSX.Element => {
+  const language_info = repo_info.languages.edges[0].node;
+  return (
+    <Fragment key={slugify(repo_info.name)}>
+      <ProjectBox
+        name={repo_info.name}
+        language={{
+          name: language_info.name,
+          colour: language_info.color,
+        }}
+        description={repo_info.description}
+      />
+    </Fragment>
+  );
+};
+
+const ProjectsPage = ({
+  data,
+}: {
+  data: {
+    githubData: {
+      data: { viewer: { repositories: { nodes: IProjectInfo[] } } };
+    };
+  };
+}): JSX.Element => (
+  <Page>
+    {data.githubData.data.viewer.repositories.nodes
+      .filter((repo_info: IProjectInfo): boolean => !repo_info.isArchived)
+      .filter(
+        (repo_info: IProjectInfo): boolean =>
+          repo_info.languages.edges.length > 0
+      )
+      .map(repo_info_to_page)}
   </Page>
 );
 
