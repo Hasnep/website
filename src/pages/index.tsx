0b3efd026896f1fe682589c7f929dd611ba60e45
Hannes <hasnep@gmail.com>
2020-07-23 19:27:05 +0100

    Move projects to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,69 @@
-import React from "react";
+import { graphql } from "gatsby";
 import Page from "../components/page";
+import ProjectBox from "../components/project-box";
+import React, { Fragment } from "react";
+import slugify from "slugify";
 
-const IndexPage = (): JSX.Element => (
-  <Page>
-    <p> Hi! :)</p>
-  </Page>
-);
+export const query = graphql`
+  query GetGithubProjectSummaries {
+    githubData {
+      data {
+        viewer {
+          repositories {
+            nodes {
+              name
+              languages {
+                edges {
+                  node {
+                    name
+                    color
+                  }
+                }
+              }
+              description
+              createdAt
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
-export default IndexPage;
+const repo_info_to_page = (repo_info: IProjectInfo): JSX.Element => {
+  const language_info = repo_info.languages.edges[0].node;
+  return (
+    <Fragment key={slugify(repo_info.name)}>
+      <ProjectBox
+        name={repo_info.name}
+        language={{
+          name: language_info.name,
+          colour: language_info.color,
+        }}
+        description={repo_info.description}
+      />
+    </Fragment>
+  );
+};
+
+const ProjectsPage = ({
+  data,
+}: {
+  data: {
+    githubData: {
+      data: { viewer: { repositories: { nodes: IProjectInfo[] } } };
+    };
+  };
+}): JSX.Element => (
+    <Page>
+      {data.githubData.data.viewer.repositories.nodes
+        .filter((repo_info: IProjectInfo): boolean => !repo_info.isArchived)
+        .filter(
+          (repo_info: IProjectInfo): boolean =>
+            repo_info.languages.edges.length > 0
+        )
+        .map(repo_info_to_page)}
+    </Page>
+  );
+
+export default ProjectsPage;
