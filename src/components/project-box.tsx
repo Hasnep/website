91ddd4a5c0f446fe4412422f38523fa6eb9a206a
Hannes <hasnep@gmail.com>
2020-07-25 12:37:44 +0100

    Fix styling on language dot

--- src/components/project-box.tsx
+++ src/components/project-box.tsx
@@ -4,19 +4,22 @@ import slugify from "../slugify";
 import { IProjectInfo } from "../interfaces/interfaces";
 
 const Dot = (props: { colour: string }): JSX.Element => (
-  <span className={"dot"} style={{ backgroundColor: props.colour }} />
+  <span
+    className={"dot"}
+    style={{ backgroundColor: props.colour, marginRight: "0.5rem" }}
+  />
 );
 
 const ProjectBox = ({ repo }: { repo: IProjectInfo }): JSX.Element => {
   return (
     <div className={"box box-hover"}>
-      <Link to={"/projects/" + slugify(repo.name)}>
+      <Link to={"/project/" + slugify(repo.name)}>
         <h2>{repo.name}</h2>
-        <div>
-          <Link to={"/projects/languages/" + slugify(repo.language.name)}>
-            <Dot colour={repo.language.colour} /> {repo.language.name}
-          </Link>
-        </div>
+        <Link to={"/language/" + slugify(repo.language.name)}>
+          <div>
+            <Dot colour={repo.language.colour} />{repo.language.name}
+          </div>
+        </Link>
         <p>{repo.description}</p>
       </Link>
     </div>
