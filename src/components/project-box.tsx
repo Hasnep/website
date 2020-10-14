4a41b052c8513642636dca5f4496e9f61fbde5a6
Hannes <hasnep@gmail.com>
2020-10-14 21:03:05 +0100

    Add an emoji box to each project

--- src/components/project-box.tsx
+++ src/components/project-box.tsx
@@ -4,16 +4,19 @@ import slugify from "../slugify";
 import { IProjectInfo } from "../interfaces/interfaces";
 import LanguageButton from "./language-button";
 
-const ProjectBox = ({ repo }: { repo: IProjectInfo }): JSX.Element => {
-  return (
-    <div className="box box-hover">
-      <Link to={"/projects/" + slugify(repo.name)} className="no-underline">
-        <h3>{repo.name}</h3>
-        <LanguageButton repo={repo} />
-        <p>{repo.description}</p>
-      </Link>
-    </div>
-  );
-};
+const EmojiBox = ({ emoji }: { emoji: string | null }): JSX.Element => (
+  <div className="emoji-box">{emoji ? emoji : ":)"}</div>
+);
+
+const ProjectBox = ({ repo }: { repo: IProjectInfo }): JSX.Element => (
+  <div className="box box-hover">
+    <Link to={"/projects/" + slugify(repo.name)} className="no-underline">
+      <EmojiBox emoji={repo.emoji} />
+      <h3>{repo.name}</h3>
+      <p>{repo.description}</p>
+      <LanguageButton repo={repo} />
+    </Link>
+  </div>
+);
 
 export default ProjectBox;
