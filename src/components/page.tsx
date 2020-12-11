aaca15181de331fb8e092fb1377bf397937efca9
Hannes <hasnep@gmail.com>
2020-12-11 00:24:06 +0000

    Add email and github links in header

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { StaticQuery, graphql, Link } from "gatsby";
+import { FaGithub, FaEnvelope } from "react-icons/fa";
 
 const WebsiteTitle = (): JSX.Element => (
   <StaticQuery
@@ -13,9 +14,25 @@ const WebsiteTitle = (): JSX.Element => (
       }
     `}
     render={(data): JSX.Element => (
-      <Link to={"/"} className="no-underline">
-        <h1>{data.site.siteMetadata.title}</h1>
-      </Link>
+      <div className="heading">
+        <Link to={"/"} className="no-underline">
+          <h1>{data.site.siteMetadata.title}</h1>
+        </Link>
+        <ul>
+          <li>
+            <a href="https://github.com/hasnep">
+              <FaGithub style={{ marginRight: "0.5rem" }} />
+              Hasnep
+            </a>
+          </li>
+          <li>
+            <a href="mailto:h@nnes.dev">
+              <FaEnvelope style={{ marginRight: "0.5rem" }} />
+              h@nnes.dev
+            </a>
+          </li>
+        </ul>
+      </div>
     )}
   />
 );
