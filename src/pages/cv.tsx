7e3986e983b424230b949b22f4141dc8d5234b9c
Hannes <hasnep@gmail.com>
2020-12-13 16:58:51 +0000

    Add CV page

---
+++ src/pages/cv.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Page from "../components/page";
+import { StaticQuery, graphql } from "gatsby";
+import ReactMarkdown from "react-markdown";
+
+interface ICVResult {
+  github: {
+    viewer: {
+      repository: {
+        cv: {
+          text: string;
+        };
+      };
+    };
+  };
+}
+
+const cv_query = graphql`
+  query CV {
+    github {
+      viewer {
+        repository(name: "cv") {
+          cv: object(expression: "main:cv-johannes-smit.md") {
+            ... on GitHub_Blob {
+              text
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const CVPage = (): JSX.Element => (
+  <Page>
+    <StaticQuery
+      query={cv_query}
+      render={(data: ICVResult): JSX.Element => (
+        <ReactMarkdown source={data.github.viewer.repository.cv.text} />
+      )}
+    />
+  </Page>
+);
+
+export default CVPage;
