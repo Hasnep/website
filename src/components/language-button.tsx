1b1e95e10c88d2ff100565b5a30b040ec850de16
Hannes <hasnep@gmail.com>
2020-12-13 21:14:25 +0000

    Remove commented code

--- src/components/language-button.tsx
+++ src/components/language-button.tsx
@@ -1,6 +1,4 @@
 import React, { Fragment } from "react";
-// import { Link } from "gatsby";
-// import slugify from "../slugify";
 import { IProjectInfo } from "../interfaces/interfaces";
 
 const Dot = (props: { colour: string }): JSX.Element => (
@@ -11,12 +9,10 @@ const Dot = (props: { colour: string }): JSX.Element => (
 );
 
 const LanguageButton = (props: { repo: IProjectInfo }): JSX.Element => (
-  //  <Link to={"/projects/languages/" + slugify(props.repo.language.name)}>
   <Fragment>
     <Dot colour={props.repo.language.colour} />
     {props.repo.language.name}
   </Fragment>
-  // </Link>
 );
 
 export default LanguageButton;
