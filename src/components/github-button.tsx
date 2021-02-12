1addb1f750b4e0e2c0efed00582bdfa9e8a38c7b
Hannes <hasnep@gmail.com>
2021-02-12 16:53:06 +0000

    Fix whitespace in GitHub button

--- src/components/github-button.tsx
+++ src/components/github-button.tsx
@@ -5,7 +5,8 @@ import ExternalLink from "./external-link";
 
 const GithubButton = ({ repo }: { repo: IProjectInfo }): JSX.Element => (
   <ExternalLink to={repo.url}>
-    <FaGithub style={{ marginRight: "0.5rem" }} /> GitHub
+    <FaGithub style={{ marginRight: "0.5rem" }} />
+    GitHub
   </ExternalLink>
 );
 
