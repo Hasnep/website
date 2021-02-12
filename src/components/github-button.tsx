1f1d493a110078f48918af68c99835a15de625f4
Hannes <hasnep@gmail.com>
2021-02-12 09:17:50 +0000

    Fix external links not opening in new tab

--- src/components/github-button.tsx
+++ src/components/github-button.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { IProjectInfo } from "../interfaces/interfaces";
-import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import ExternalLink from "./external-link";
 
 const GithubButton = ({ repo }: { repo: IProjectInfo }): JSX.Element => (
-  <a href={repo.url}>
-    <FaGithub style={{ marginRight: "0.5rem" }} />
-    GitHub
-    <FaExternalLinkAlt style={{ marginLeft: "0.5rem" }} />
-  </a>
+  <ExternalLink to={repo.url}>
+    <FaGithub style={{ marginRight: "0.5rem" }} /> GitHub
+  </ExternalLink>
 );
 
 export default GithubButton;
