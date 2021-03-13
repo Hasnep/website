02e25ef27cf20148cdda43b9ba29768ad979cffa
Hannes <hasnep@gmail.com>
2021-03-13 19:43:57 +0000

    Simplify projectbox grid using scss

--- src/components/project-box.tsx
+++ src/components/project-box.tsx
@@ -1,13 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "gatsby";
 import slugify from "../slugify";
 import { IProjectInfo } from "../interfaces/interfaces";
 import LanguageButton from "./language-button";
 import GithubButton from "./github-button";
-
-const EmojiBox = ({ emoji }: { emoji: string | null }): JSX.Element => (
-  <div className="emoji-box">{emoji ? emoji : ":)"}</div>
-);
 
 const ProjectBox = ({
   repo,
@@ -16,27 +12,26 @@ const ProjectBox = ({
   repo: IProjectInfo;
   detailed: boolean;
 }): JSX.Element => {
-  let title = <h3 className="project-title">{repo.name}</h3>;
-  let emoji_box = <EmojiBox emoji={repo.emoji} />;
+  let title = <Fragment> {repo.name} </Fragment>;
+  let emoji_box = <Fragment> {repo.emoji ? repo.emoji : ":)"} </Fragment>;
+
+  // Add links if detailed
   if (detailed) {
     const link_to = "/projects/" + slugify(repo.name);
     title = <Link to={link_to}>{title}</Link>;
     emoji_box = <Link to={link_to}>{emoji_box}</Link>;
   }
+
   return (
     <div className="box project drop-shadow">
-      {emoji_box}
-      <div className="column">
-        {title}
-        <p>{repo.description}.</p>
-        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-          <div>
-            Language: <LanguageButton repo={repo} />
-          </div>
-          <div>
-            Source code: <GithubButton repo={repo} />
-          </div>
-        </div>
+      <div className="emoji-box">{emoji_box}</div>
+      <h3 className="project-title">{title}</h3>
+      <p className="project-description">{repo.description}.</p>
+      <div className="project-language">
+        Language: <LanguageButton repo={repo} />
+      </div>
+      <div className="project-github-button">
+        Source code: <GithubButton repo={repo} />
       </div>
     </div>
   );
