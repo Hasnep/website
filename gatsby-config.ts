6876c0f4da1ed4abf75331366c273e3920c1060d
Hannes <hasnep@gmail.com>
2021-03-13 19:16:50 +0000

    Add error checking to environment variable

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
+const getGitHubAPIToken = (): string => {
+  const token = process.env.GITHUB_API_TOKEN;
+  if (token === undefined) {
+    console.error("Environment variable GITHUB_API_TOKEN not defined.");
+    process.exit(1);
+  } else {
+    return token;
+  }
+};
+
 export const siteMetadata = {
   title: "Ha.nnes.dev",
   description: "My website to show off some of my projects.",
@@ -17,7 +27,7 @@ export const plugins = [
       typeName: "GitHub",
       fieldName: "github",
       url: "https://api.github.com/graphql",
-      headers: { Authorization: `bearer ${process.env.GITHUB_API_TOKEN}` },
+      headers: { Authorization: `bearer ${getGitHubAPIToken()}` },
     },
   },
   // Temporarily broken
