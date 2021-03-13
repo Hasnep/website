f4dd6b3fc1e2cb8e962741cbabe6fd0a904ab47a
Hannes <hasnep@gmail.com>
2021-03-13 18:38:25 +0000

    Change footer div to footer tag

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Footer = (): JSX.Element => (
-  <div className="footer">
+  <footer>
     <div>
       Favicon and other icons from{" "}
       <a href="http://fontawesome.io">Font Awesome by Dave Gandy</a>.
     </div>
-  </div>
+  </footer>
 );
 
 export default Footer;
