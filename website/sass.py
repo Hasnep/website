3526b3e97670873f03b613338f2f03ff7580ee83
Hannes <h@nnes.dev>
2025-12-29 08:36:14 +0800

    Replace dart-sass with grass-sass

--- website/sass.py
+++ website/sass.py
@@ -2,4 +2,4 @@ from website.utils import run_cli_command
 
 
 def compile_sass(sass: str) -> str:
-    return run_cli_command(["dart-sass", "--stdin"], stdin=sass)
+    return run_cli_command(["grass", "--stdin"], stdin=sass)
